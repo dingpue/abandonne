18dfc0894a2da9c292aacac787a2023905295d57
dami-kim <62894564+dami-kim@users.noreply.github.com>
2020-12-16 14:35:21 +0900

    Update layoutHandler.js

--- js/layoutHandler.js
+++ js/layoutHandler.js
@@ -2,7 +2,7 @@ function layoutHandler() {
     var styleLink = document.getElementById("pagestyle");
     if(window.innerWidth < 950) {
       // styleLink.setAttribute("href", "mobile.css");
-      alert("데스크탑으로 방문해주세요. 데스크탑이셨다면 윈도우 크기를 넓혀주세요.");
+      document.location= "mobile.html";
     } else {
       styleLink.setAttribute("href", "desktop.css");
     }
