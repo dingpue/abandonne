f604e50cb55c34e89fc8fd9cd8e348f4ff0510eb
dami-kim <62894564+dami-kim@users.noreply.github.com>
2022-01-28 17:28:58 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+var config = {
+  MAP_FR_KEY = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCFPZtyZJoAJtnYcwXmVePsWHh-O2lyBwE&region=FR&language=fr&callback=myMap',
+  MAP_KO_KEY = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCFPZtyZJoAJtnYcwXmVePsWHh-O2lyBwE&region=KO&language=ko&callback=myMap'
+}
