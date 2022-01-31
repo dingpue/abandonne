a915b24f5bea7307fe63c5fb124707c8f1eb4e6a
dami-kim <62894564+dami-kim@users.noreply.github.com>
2022-01-31 17:31:38 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 var config = {
-  MAP_FR_KEY = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCGLshRaM4IIbbth74yEjQtKsLwYtHmIJc&region=FR&language=fr&callback=myMap',
-  MAP_KO_KEY = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCGLshRaM4IIbbth74yEjQtKsLwYtHmIJc&region=KO&language=ko&callback=myMap'
+  MAP_FR_KEY : 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCGLshRaM4IIbbth74yEjQtKsLwYtHmIJc&region=FR&language=fr&callback=myMap',
+  MAP_KO_KEY : 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCGLshRaM4IIbbth74yEjQtKsLwYtHmIJc&region=KO&language=ko&callback=myMap'
 }
