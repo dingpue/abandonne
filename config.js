8f2b4f9e56568157f3d9e9c6caf9cbaffaac6de0
dami-kim <62894564+dami-kim@users.noreply.github.com>
2022-03-17 00:14:28 +0900

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-    MAP_FR_KEY : 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCGLshRaM4IIbbth74yEjQtKsLwYtHmIJc&region=FR&language=fr&callback=myMap',
-    MAP_KO_KEY : 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCGLshRaM4IIbbth74yEjQtKsLwYtHmIJc&region=KO&language=ko&callback=myMap'
-  }
-
